da7007ec6ac13d239fc5c5ab849c13f8aba1223d
Guamoy <georgyvino@gmail.com>
2023-09-25 20:00:49 -0500

    Added db credentials to settings.py

--- Gemukyu/settings.py
+++ Gemukyu/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "Gemukyu.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gemukyu-db',
+        'USER': 'admin',
+        'PASSWORD': '8302129016',
+        'HOST': 'gemukyu-db.czghdsoor9r5.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
