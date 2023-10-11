6d8fca6d33e39a7bce4bfadd91b13b1eb87c964a
Kimberly Flores <kimberlyflores.me@gmail.com>
2023-10-11 13:50:30 -0500

    hide horizonal scroll home

--- Gemukyu/settings.py
+++ Gemukyu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8rzz2=_7xwvc*hjfvt+q(28-@wyhpj3hxqkii#48$nu^g%wzol"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', 
                  'https://www.kimberlyjflores.github.io/Gemukyu/',
