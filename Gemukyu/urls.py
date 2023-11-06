2cf85a591cb7f5397a1fdf456ea4d937401a2c56
Kimberly Flores <92899472+KimberlyJFlores@users.noreply.github.com>
2023-11-06 10:17:06 -0600

    Register (#8)
    * fixes
    
    * fix
    
    * registration page css
    
    * change
    
    ---------
    
    Co-authored-by: AndMan9919 <roomboy222@gmail.com>

--- Gemukyu/urls.py
+++ Gemukyu/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("login_user", views.login_user, name="login_user"),
     path("logout_user", views.logout_user, name="logout_user"),
+    path('account/',views.account_page,name='account'),
     ]
 urlpatterns += staticfiles_urlpatterns()
